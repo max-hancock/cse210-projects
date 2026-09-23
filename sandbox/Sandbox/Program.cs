50533241a625b10104519c5a07d36523b3131bf2
max-hancock <maxhancock64934@gmali.com>
2026-09-22 18:44:24 -0600

    test

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -2,10 +2,26 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main(string[] args) // static = function has no class, 
+    // void = function tells compiler no value will be returned
     {
-        Console.WriteLine("bugdeeree sain baitsganuu!");
-        Console.WriteLine("Hello World!");
+       int x = 20;
+       int y = 20;
+       
+       if (x == 10)
+       {
+        Console.WriteLine("X is 10");
+        Console.WriteLine("Y is fun");
+       }
+
+       else if (x == 20)
+        {
+            Console.WriteLine("We are in an else if.");
+        }
+
+       else
+        {
+            Console.WriteLine("Z is not much fun");
+        }
     }
 }
-
