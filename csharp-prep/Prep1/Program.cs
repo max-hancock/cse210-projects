b15b903a907eb401b15d125e92e9d8907c3358f3
max-hancock <maxhancock64934@gmali.com>
2026-09-22 18:41:08 -0600

    Completed code for W01 Prove: Developer—C# Programming Parts 1–2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -2,7 +2,8 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main(string[] args) // static = function has no class, 
+    // void = function tells compiler no value will be returned
     {
         string firstName;
         string lastName;
@@ -11,6 +12,6 @@ class Program
         Console.Write("Please enter your last name: ");
         lastName = Console.ReadLine();
         
-        Console.WriteLine($"Hello {firstName} {lastName}!");
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Runtime.Intrinsics.X86;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+
+        Console.Write("Please enter course grade out of 100: ");
+        float studentgrade = float.Parse(Console.ReadLine());
+
+        if (studentgrade >= 90)
+        {
+            Console.WriteLine("You got an A!");
+            Console.WriteLine("Congratulations, you passed the course.");
+        }
+
+        else if (studentgrade <= 89 && studentgrade >= 80)
+        {
+            Console.WriteLine("You got a B");
+            Console.WriteLine("Congratulations, you passed the course.");
+        }
+
+        else if (studentgrade <= 79 && studentgrade >= 70)
+        {
+            Console.WriteLine("You got a C");
+            Console.WriteLine("Congratulations, you passed the course.");
+        }
+
+        else if (studentgrade <= 69 && studentgrade >= 60)
+        {
+            Console.WriteLine("You got a D");
+            Console.WriteLine("Congratulations, you passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you failed the Course.");
+        }
+
+}
 }
